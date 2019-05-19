8c3f7f04fc972fe3c7370c835593a9f92a35d6e5
Moussa Sarr <moussasarr490@gmail.com>
2019-05-19 14:37:21 -0400

    polynomial regression

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -9,10 +9,26 @@ for line in open("./data/data_poly.csv"):
 
 	x = float(x)
 	X.append([1, x, x*x])
-
-    y = float(y)
+	y = float(y)
 	Y.append(y)
 
+X = np.array(X)
+Y = np.array(Y)
 
-	print(X)
+plt.scatter(X[:, 1], Y)
+plt.show()
+
+W = np.linalg.solve(X.T.dot(X), X.T.dot(Y))
+Yhat = X.dot(W.T) 
+
+plt.scatter(sorted(X[:, 1]), sorted(Y))
+plt.plot(sorted(X[:, 1]), sorted(Yhat))
+plt.show()
+
+dt1 =  Y - Yhat
+dt2 =  Y - Y.mean()
+
+R2 = 1 - (dt1.dot(dt1)).sum() / dt2.dot(dt2).sum()
+print("R-square value is ", R2)
+
 
